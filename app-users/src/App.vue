c8a86253628af71cc702d1eb036bfee442b7d89e
Manuel <TeNNoX@users.noreply.github.com>
2019-04-13 12:54:30 +0200

    Blockchain connection alert

--- app-users/src/App.vue
+++ app-users/src/App.vue
@@ -4,15 +4,11 @@
 
     <v-content>
       <v-container fluid>
-        <h2 v-show="!bcConnected && !bcConnectionError" class="p-4">
+        <v-alert :value="!bcConnected && !bcConnectionError" type="info">
           Connecting to Blockchain...
-        </h2>
-
-        <div v-show="bcConnected && !bcConnectionError" class="p-4">
-          <router-view />
-        </div>
-
-        <div v-show="bcConnectionError" class="m-4 alert alert-danger">
+          <v-progress-linear :indeterminate="true" />
+        </v-alert>
+        <v-alert :value="bcConnectionError" type="error">
           <h2 class="pb-4">
             Error connecting to the blockchain!
           </h2>
@@ -25,14 +21,20 @@
             <b>It seems like the address of the smart contract is wrong!</b>
           </p>
 
-          <hr>
-
           <p>Other common causes of error:</p>
           <ul>
             <li>The blockchain is running.</li>
             <li>The port in your settings (file: <b>libs/mixinViews.js</b>) match with the blockchain configuration.</li>
             <li>The compiled smart contract <b>app-users/src/assets/contracts/Users.json</b> is not up to date with <b>build/Users.json</b>.</li>
           </ul>
+
+          <v-btn onclick="window.location.reload(false);">
+            Retry
+          </v-btn>
+        </v-alert>
+
+        <div v-show="bcConnected && !bcConnectionError" class="p-4">
+          <router-view />
         </div>
       </v-container>
     </v-content>
